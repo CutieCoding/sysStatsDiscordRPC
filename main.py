93830239edef60bccd4191030d898156105f47e8
CutieCoding <no-bruh@proton.me>
2023-01-14 12:06:32 +0000

    add readme.md, add comments to main.py

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from pypresence import Presence
 import time
 import psutil
 
-def grabCPU():
-    return str(psutil.cpu_percent()) + "%" # why is this so dogshit lmao
+def grabCPU(): #function to grab cpu usage percent as a string
+    return str(psutil.cpu_percent()) + "%"
 
-def grabRAM():
+def grabRAM(): #function to grab ram usage percent as a string
     return str(psutil.virtual_memory().percent) + "%"
 
-def readClientID():
+def readClientID(): #function to open client_id.txt and read whats inside, then return it
     with open("client_id.txt") as x:
         clientID = x.readline().strip()
     return clientID
 
-client_id = readClientID()  # Put your Client ID in client_id.txt
+client_id = readClientID()  #put your client id in client_id.txt
 RPC = Presence(client_id)  # Initialize the Presence client
 RPC.connect() # Start the handshake loop
 
-while True:  # The presence will stay on as long as the program is running
+while True:  # The presence will stay on as long as the program is running (in this case, infinitely)
     RPC.update(state = "RAM Usage: " + grabRAM(), details="CPU Usage: " + grabCPU()) # Updates our presence
     time.sleep(15) # Can only update rich presence every 15 seconds
